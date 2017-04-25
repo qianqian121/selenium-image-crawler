2289fbf60c3de1a458b0a308203694c5085cbd76
qianqian121 <qianqian141@hotmail.com>
2017-04-25 15:38:13 -0700

    fix yandex
    fix yandex

--- crawler/YandexCrawler.py
+++ crawler/YandexCrawler.py
@@ -39,27 +39,51 @@ class YandexCrawler(BaseCrawler):
                 "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
             # driver.save_screenshot('out.png')
             counter = 0
+            counter1 = 0
             while True:
                 doc_height_new = driver.execute_script(
                     "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-                button_load_rest = driver.find_element_by_class_name(self.show_more_find_value)
-                if button_load_rest and button_load_rest.is_displayed():
-                    button_load_rest.click()
-                    time.sleep(0.5)
-                    doc_height_new = driver.execute_script(
-                        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-                if doc_height == doc_height_new:
-                    button_load_rest = driver.find_element_by_class_name(self.show_more_find_value)
-                    if button_load_rest is None:
-                        break
-                    else:
-                        counter += 1
-                        if counter > 10:
-                            break
-                else:
-                    doc_height = doc_height_new
-                    counter = 0
-                time.sleep(0.1)
+                button_load_rest = driver.find_element_by_xpath("/html/body/div[6]/div[1]/div/div[3]/a")
+                if button_load_rest is None:
+                    break
+                if button_load_rest.is_displayed() != True:
+                     time.sleep(2)
+                     print("waiting 2 seconds for clickable button...")
+                     counter1 +=1
+                     if counter1 > 10:
+                         print("page load comopleted, end of page")
+                         break;
+                     continue
+
+                print(doc_height_new)
+                filename = "output" + str(counter) + ".html"
+                target = open(filename, 'w')
+                target.write(str(driver.page_source.encode('ascii', 'ignore')))
+                target.close()
+
+                button_load_rest.click()
+                print("button clicked")
+
+                counter += 1
+                counter1 = 0
+
+                #if button_load_rest and button_load_rest.is_displayed():
+                #    button_load_rest.click()
+                #    time.sleep(0.5)
+                #    doc_height_new = driver.execute_script(
+                #        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+                #if doc_height == doc_height_new:
+                #    button_load_rest = driver.find_element_by_xpath("/html/body/div[6]/div[1]/div/div[3]/a")
+                #    if button_load_rest is None:
+                #        break
+                #    else:
+                #        counter += 1
+                #        if counter > 10:
+                #            break
+                #else:
+                #    doc_height = doc_height_new
+                #    counter = 0
+                #time.sleep(0.1)
 
         except Exception as e:
             print(e)
--- example_yandex.py
+++ example_yandex.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         'output_directory': './,'
     }
     # PYD is a European Union supported terrorist organization against Turkish Government
-    w = YandexCrawler(search_key='PYD')
+    w = YandexCrawler(search_key='traffic cone')
     w.append_processor(LogProcessor())
     # w.append_processor(DownloadProcessor(output_directory=options['output_directory'], process_count=16))
     w.run()
